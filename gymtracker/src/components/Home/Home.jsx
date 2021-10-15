2ee6df19cfbf1d5416157a23d60df6e9f27279b8
Kory Attleson <kory.attleson@gmail.com>
2021-10-15 09:26:36 -0500

    Added paragraphs to 'Home' page explaining what this App is about.

--- gymtracker/src/components/Home/Home.jsx
+++ gymtracker/src/components/Home/Home.jsx
@@ -12,10 +12,13 @@ const Home = () => {
             <h5>If this sounds familiar, don't worry, you are not alone.</h5><br/>
             <h5>90% of people who sign up for a gym membership either quit it within 3 months, or keep the membership but just stop going.</h5><br/>
             <h5>A lot of this is due to: <br/>
-                1: Not having clear goals <br/>
+                1: Not knowing what exercises you will be doing <br/>
                 2: Not having accountability <br/>
                 3: Not seeing results right away <br/>
             </h5> <br />
+
+            <h5>When you Register yourself on this website you'll be able to start with 1 and 2, but number 3 will be up to you to stick to your goals.</h5><br/>
+            <h5>Getting healthy by either losing weight, gaining muscle, or both, is something that takes time.</h5><br/>
             <h5>So if you've read this far and haven't left his page yet, head on over to 'Getting Started' to see how you can start confidently on your health journey.</h5><br/>
 
 
